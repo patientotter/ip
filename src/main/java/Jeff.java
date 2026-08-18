538a5f113003678a1fe88966c10ca1c8d361170b
patientotter <kerjun04@gmail.com>
2026-08-18 22:46:42 +0800

    Complete Level-0

---
+++ src/main/java/Jeff.java
@@ -0,0 +1,17 @@
+public class Jeff {
+    public static void main(String[] args) {
+        String banner = "     _ _____ _____ _____ \n"
+                + "    | | ____|  ___|  ___|\n"
+                + " _  | |  _| | |_  | |_   \n"
+                + "| |_| | |___|  _| |  _|  \n"
+                + " \\___/|_____|_|   |_|    \n";
+
+        System.out.println("____________________________________________________________");
+        System.out.print(banner);
+        System.out.println("Hello! I'm Jeff.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
