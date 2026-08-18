ca84d8d6e66cbfbfe3628681d5309d692cb7cf55
patientotter <kerjun04@gmail.com>
2026-08-18 23:39:19 +0800

    Complete Level-1

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Jeff {
     public static void main(String[] args) {
         String banner = "     _ _____ _____ _____ \n"
@@ -11,7 +12,32 @@ public class Jeff {
         System.out.println("Hello! I'm Jeff.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String[] tasks = new String[100];
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            System.out.println("____________________________________________________________");
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[count] = input;
+                count++;
+                System.out.println(" added: " + input);
+            }
+
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
