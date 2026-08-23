e35db876713248e7625b36cd835558999411680f
patientotter <kerjun04@gmail.com>
2026-08-24 01:40:27 +0800

    Refactor Jeff into application coordinator

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,19 +1,30 @@
 import java.io.IOException;
 
 public class Jeff {
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        Storage storage = new Storage("data/duke.txt");
-        ui.showWelcome();
+    private final Storage storage;
+    private final TaskList tasks;
+    private final Ui ui;
 
-        TaskList tasks;
+    public Jeff(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+
+        TaskList loadedTasks;
 
         try {
-            tasks = new TaskList(storage.loadTasks());
+            loadedTasks = new TaskList(storage.loadTasks());
         } catch (IOException e) {
-            ui.showMessage("Unable to load saved tasks. Starting with an empty list.");
-            tasks = new TaskList();
+            ui.showMessage(
+                    "Unable to load saved tasks. "
+                            + "Starting with an empty list.");
+            loadedTasks = new TaskList();
         }
+
+        tasks = loadedTasks;
+    }
+
+    public void run() {
+        ui.showWelcome();
 
         while (true) {
             String input = ui.readCommand();
@@ -22,109 +33,23 @@ public class Jeff {
             ui.showLine();
 
             if (command.equals("bye")) {
-                //says goodbye (task 0)
                 ui.showGoodbye();
                 ui.showLine();
                 break;
-            } else if (input.equals("list")) {
-                //task 4
-                ui.showMessage("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    ui.showMessage(" " + (i + 1) + "." + tasks.get(i));
-                }
+            } else if (command.equals("list")) {
+                showTaskList();
             } else if (command.equals("mark")) {
-                try {
-                    int taskNumber = Parser.parseTaskNumber(input);
-
-                    if (taskNumber < 1 || taskNumber > tasks.size()) {
-                        ui.showMessage("OOPS! That task number does not exist.");
-                    } else {
-                        Task task = tasks.get(taskNumber - 1);
-                        task.markAsDone();
-                        saveTasks(tasks, ui, storage);
-
-                        ui.showMessage("Nice! I've marked this task as done:");
-                        ui.showMessage("  " + task.getStatus()
-                                + " " + task.getDesc());
-                    }
-                } catch (IllegalArgumentException e) {
-                    ui.showMessage(e.getMessage());
-                }
+                markTask(input);
             } else if (command.equals("unmark")) {
-                try {
-                    int taskNumber = Parser.parseTaskNumber(input);
-
-                    if (taskNumber < 1 || taskNumber > tasks.size()) {
-                        ui.showMessage("That task number does not exist.");
-                    } else {
-                        Task task = tasks.get(taskNumber - 1);
-                        task.unmarkAsDone();
-                        saveTasks(tasks, ui, storage);
-
-                        ui.showMessage("I've marked this task as undone:");
-                        ui.showMessage("  " + task.getStatus()
-                                + " " + task.getDesc());
-                    }
-                } catch (IllegalArgumentException e) {
-                    ui.showMessage(e.getMessage());
-                }
+                unmarkTask(input);
             } else if (command.equals("todo")) {
-                try {
-                    Todo todo = Parser.parseTodo(input);
-                    tasks.add(todo);
-                    saveTasks(tasks, ui, storage);
-
-                    ui.showMessage("Got it. I've added this task:");
-                    ui.showMessage("  " + todo);
-                    ui.showMessage("Now you have " + tasks.size()
-                            + " tasks in the list.");
-                } catch (IllegalArgumentException e) {
-                    ui.showMessage(e.getMessage());
-                }
+                addTodo(input);
             } else if (command.equals("deadline")) {
-                try {
-                    Deadline deadline = Parser.parseDeadline(input);
-                    tasks.add(deadline);
-                    saveTasks(tasks, ui, storage);
-
-                    ui.showMessage("Got it. I've added this task:");
-                    ui.showMessage("  " + deadline);
-                    ui.showMessage("Now you have " + tasks.size()
-                            + " tasks in the list.");
-                } catch (IllegalArgumentException e) {
-                    ui.showMessage(e.getMessage());
-                }
+                addDeadline(input);
             } else if (command.equals("event")) {
-                try {
-                    Event event = Parser.parseEvent(input);
-                    tasks.add(event);
-                    saveTasks(tasks, ui, storage);
-
-                    ui.showMessage("Got it. I've added this task:");
-                    ui.showMessage("  " + event);
-                    ui.showMessage("Now you have " + tasks.size()
-                            + " tasks in the list.");
-                } catch (IllegalArgumentException e) {
-                    ui.showMessage(e.getMessage());
-                }
+                addEvent(input);
             } else if (command.equals("delete")) {
-                try {
-                    int taskNumber = Parser.parseTaskNumber(input);
-
-                    if (taskNumber < 1 || taskNumber > tasks.size()) {
-                        ui.showMessage("That task number does not exist.");
-                    } else {
-                        Task removedTask = tasks.delete(taskNumber - 1);
-                        saveTasks(tasks, ui, storage);
-
-                        ui.showMessage("Noted. I've removed this task:");
-                        ui.showMessage("  " + removedTask);
-                        ui.showMessage("Now you have " + tasks.size()
-                                + " tasks in the list.");
-                    }
-                } catch (IllegalArgumentException e) {
-                    ui.showMessage(e.getMessage());
-                }
+                deleteTask(input);
             } else {
                 ui.showMessage("Invalid command.");
             }
@@ -135,11 +60,144 @@ public class Jeff {
         ui.close();
     }
 
-    private static void saveTasks(TaskList tasks, Ui ui, Storage storage) {
+    private void showTaskList() {
+        ui.showMessage("Here are the tasks in your list:");
+
+        for (int i = 0; i < tasks.size(); i++) {
+            ui.showMessage(" " + (i + 1) + "." + tasks.get(i));
+        }
+    }
+
+    private void markTask(String input) {
+        try {
+            int taskNumber = Parser.parseTaskNumber(input);
+
+            if (!isValidTaskNumber(taskNumber)) {
+                ui.showMessage(
+                        "OOPS! That task number does not exist.");
+                return;
+            }
+
+            Task task = tasks.get(taskNumber - 1);
+            task.markAsDone();
+            saveTasks();
+
+            ui.showMessage(
+                    "Nice! I've marked this task as done:");
+            ui.showMessage(
+                    "  " + task.getStatus()
+                            + " " + task.getDesc());
+        } catch (IllegalArgumentException e) {
+            ui.showMessage(e.getMessage());
+        }
+    }
+
+    private void unmarkTask(String input) {
+        try {
+            int taskNumber = Parser.parseTaskNumber(input);
+
+            if (!isValidTaskNumber(taskNumber)) {
+                ui.showMessage(
+                        "That task number does not exist.");
+                return;
+            }
+
+            Task task = tasks.get(taskNumber - 1);
+            task.unmarkAsDone();
+            saveTasks();
+
+            ui.showMessage(
+                    "I've marked this task as undone:");
+            ui.showMessage(
+                    "  " + task.getStatus()
+                            + " " + task.getDesc());
+        } catch (IllegalArgumentException e) {
+            ui.showMessage(e.getMessage());
+        }
+    }
+
+    private void addTodo(String input) {
+        try {
+            Todo todo = Parser.parseTodo(input);
+            tasks.add(todo);
+            saveTasks();
+
+            showTaskAdded(todo);
+        } catch (IllegalArgumentException e) {
+            ui.showMessage(e.getMessage());
+        }
+    }
+
+    private void addDeadline(String input) {
+        try {
+            Deadline deadline = Parser.parseDeadline(input);
+            tasks.add(deadline);
+            saveTasks();
+
+            showTaskAdded(deadline);
+        } catch (IllegalArgumentException e) {
+            ui.showMessage(e.getMessage());
+        }
+    }
+
+    private void addEvent(String input) {
+        try {
+            Event event = Parser.parseEvent(input);
+            tasks.add(event);
+            saveTasks();
+
+            showTaskAdded(event);
+        } catch (IllegalArgumentException e) {
+            ui.showMessage(e.getMessage());
+        }
+    }
+
+    private void deleteTask(String input) {
+        try {
+            int taskNumber = Parser.parseTaskNumber(input);
+
+            if (!isValidTaskNumber(taskNumber)) {
+                ui.showMessage(
+                        "That task number does not exist.");
+                return;
+            }
+
+            Task removedTask = tasks.delete(taskNumber - 1);
+            saveTasks();
+
+            ui.showMessage("Noted. I've removed this task:");
+            ui.showMessage("  " + removedTask);
+            showTaskCount();
+        } catch (IllegalArgumentException e) {
+            ui.showMessage(e.getMessage());
+        }
+    }
+
+    private boolean isValidTaskNumber(int taskNumber) {
+        return taskNumber >= 1 && taskNumber <= tasks.size();
+    }
+
+    private void showTaskAdded(Task task) {
+        ui.showMessage("Got it. I've added this task:");
+        ui.showMessage("  " + task);
+        showTaskCount();
+    }
+
+    private void showTaskCount() {
+        ui.showMessage(
+                "Now you have " + tasks.size()
+                        + " tasks in the list.");
+    }
+
+    private void saveTasks() {
         try {
             storage.saveTasks(tasks.getAllTasks());
         } catch (IOException e) {
             ui.showMessage("Unable to save tasks.");
         }
+    }
+
+    public static void main(String[] args) {
+        new Jeff("data/duke.txt").run();
     }
 }
